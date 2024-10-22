ca7118e3afc8c7e1d6009c113103faf3ab07c472
diego1216 <157553008+diego1216@users.noreply.github.com>
2024-10-22 02:35:00 +0000

    se modificó el AppBar.tsx para que se pueda actualizar el main

--- src/Components/Appbar.tsx
+++ src/Components/Appbar.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/AppBar.css';
+import '../Styles/Appbar.css';
 
 /* Se añadieoron las rutas con el plugin de react-router-dom */
 
 const AppBar: React.FC = () => {
   return (
-    <header className="full-width-bar">
-      <h1>My E-commerce - PetShop</h1>
-      <p>¡Los mejores productos para mascotas disponibles!</p>
-      <nav>
-        <ul>
+    <header className="app-bar">
+      <div className="app-bar-content">
+        <div className="logo-section">
+        <img src="/images/daysi.png" alt="Daysi Logo" className="title-image" />
+         
+        </div>
+        <div className="navbar-search">
+          <input type="text" placeholder="Buscar..." />
+        </div>
+        <nav className="navbar-icons">
+          <ul>
           <li>
-            <Link to="/add-product">Añadir Producto</Link>
-          </li>
-          <li>
-            <Link to="/products">Productos Disponibles</Link>
-          </li>
-          <li>
-            <Link to="/cart">Tu Carrito</Link>
-          </li>
-        </ul>
-      </nav>
+              <Link to="/perfile">
+              <img src="/images/perfil.svg" alt="perfile" className="icon" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/add-product">
+              <img src="images/add.svg" alt="add" className="icon" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/products">
+              <img src="images/bag.svg" alt="bag" className="icon" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart">
+              <img src="images/cart-outline.svg" alt="cart" className="icon" />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
+
+
   );
 };
 
