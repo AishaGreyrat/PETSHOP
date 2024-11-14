107ac10335a9fe553f9473d11866de1f6e75b0cb
Alvaro Renan Ek Lugo <157553181+AlvaroEk@users.noreply.github.com>
2024-11-13 19:01:19 -0600

    Agregacion de metodos de pago
    - se arreglo el app.tx que no dejaba crear nuevas pestañas
    - se arreglo cart.tx que no estaba en una pestaña diferente
    - se agrego los metodos de pago

--- src/Routes/ReactRouter.tsx
+++ src/Routes/ReactRouter.tsx
@@ -1,3 +1,4 @@
+// Routes/ReactRouter.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductForm from '../Components/ProductForm';
@@ -6,13 +7,14 @@ import Cart from '../Components/Cart';
 import AppBar from '../Components/Appbar';
 import Login from '../Components/Login';
 import Register from '../Components/Register';
+import PaymentPage from '../Components/Payment'; // Importa PaymentPage
 
-type ReactRouterProps = {
+interface ReactRouterProps {
   searchTerm: string;
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
   selectedCategory: string;
   setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
-};
+}
 
 const ReactRouter: React.FC<ReactRouterProps> = ({
   searchTerm,
@@ -42,6 +44,7 @@ const ReactRouter: React.FC<ReactRouterProps> = ({
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/payment" element={<PaymentPage />} /> {/* Nueva ruta para la página de pago */}
       </Routes>
     </Router>
   );
