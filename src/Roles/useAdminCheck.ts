3f1bf2311eb3d3482f87444af18aeb20d90e9db4
Alvaro Renan Ek Lugo <157553181+AlvaroEk@users.noreply.github.com>
2025-01-06 11:46:30 -0600

    se acorrigio errores
    -se reparo el bug que al recargar la pagina no dectetaba si eres admin o no

--- src/Roles/useAdminCheck.ts
+++ src/Roles/useAdminCheck.ts
@@ -1,32 +1,41 @@
-// src/hooks/useAdminCheck.ts
-
 import { useState, useEffect } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // Correos electrónicos de las cuentas administradoras
 const adminEmails = ["lord128@gmail.com", "admin2@example.com"];
 
 export const useAdminCheck = () => {
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const auth = getAuth();
 
   useEffect(() => {
-    const checkAdminStatus = async () => {
-      const user = auth.currentUser;
+    // Comprobar si el usuario está autenticado y es admin
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // Verifica si el correo del usuario está en la lista de administradores
-        if (adminEmails.includes(user.email || "")) {
-          setIsAdmin(true);
-        } else {
-          setIsAdmin(false);
-        }
+        const adminStatus = adminEmails.includes(user.email || "");
+        setIsAdmin(adminStatus);
+        localStorage.setItem("isAdmin", JSON.stringify(adminStatus)); // Guardamos en localStorage
+      } else {
+        setIsAdmin(false);
+        localStorage.setItem("isAdmin", JSON.stringify(false)); // Asegura que se elimine el estado de admin cuando no hay usuario
       }
       setLoading(false);
-    };
+    });
 
-    checkAdminStatus();
+    // Limpiar el listener cuando el componente se desmonte
+    return () => unsubscribe();
   }, [auth]);
+
+  // Al cargar, verificar si el estado de administrador está guardado en localStorage
+  useEffect(() => {
+    const storedIsAdmin = localStorage.getItem("isAdmin");
+    if (storedIsAdmin) {
+      setIsAdmin(JSON.parse(storedIsAdmin));
+      setLoading(false);
+    }
+  }, []);
 
   return { isAdmin, loading };
 };
