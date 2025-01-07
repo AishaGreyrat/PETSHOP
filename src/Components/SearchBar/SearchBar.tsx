0be6c39261925c93c5446d6c80f23aa47ce7343c
Alvaro Renan Ek Lugo <157553181+AlvaroEk@users.noreply.github.com>
2025-01-07 11:37:49 -0600

    se agrego cierre de categoria
    -al dar un click afuera de categoría se cierra

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import styles from "./SearchBar.module.css";
 import useMediaQuery from "@/Hooks/useMediaQuery";
@@ -34,6 +34,26 @@ const SearchBar: React.FC<SearchBarProps> = ({
   // Detectar si la pantalla es mayor o igual a 768px
   const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
 
+  // Ref para el contenedor del dropdown
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  // Manejar clics fuera del dropdown
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false); // Cerrar el dropdown si el clic es fuera del contenedor
+      }
+    };
+
+    // Agregar event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Limpiar el event listener cuando el componente se desmonte
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.navbarsearch}>
       {/* Barra de búsqueda */}
@@ -47,7 +67,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
 
       {/* Botón de categorías visible solo en pantallas grandes */}
       {isAboveMediumScreens && (
-        <div className={styles["dropdown-container"]}>
+        <div className={styles["dropdown-container"]} ref={dropdownRef}>
           <button
             className={styles["dropdown-button"]}
             onClick={() => setIsDropdownOpen(!isDropdownOpen)}
