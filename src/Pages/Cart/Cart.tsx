f7a4a2034aac6668e52ac0f89450b7cc83f65dfb
diego1216 <157553008+diego1216@users.noreply.github.com>
2025-01-08 13:40:07 -0600

    carrito vació
    se corrigió el carrito vació

--- src/Pages/Cart/Cart.tsx
+++ src/Pages/Cart/Cart.tsx
@@ -3,10 +3,7 @@ import { useCart } from '@/Contexts/CartContext';
 import { useNavigate } from 'react-router-dom';
 import { handleRemove, handleClearCart } from '@/Utils/utils';
 
-import style from "./Cart.module.css";
-import "./Cart.module.css";
-
-import '@/Styles/AppBar.css';
+import styles from "./Cart.module.css";
 
 const Cart: React.FC = () => {
   const { state, dispatch } = useCart();
@@ -17,53 +14,53 @@ const Cart: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="empty-cart-container">
-      <h2 className="carritotitle">Tu carrito</h2>
+    <div >
+      <h2 className={styles.carritotitle} >Tu carrito</h2>
       {state.items.length === 0 ? (
         <div>
-          <h2 >
+          <h2 className={styles["Carrito-vacío"]} >
             Tu carrito está vacío
           </h2>
           <img
-            className="cat"
+            className={styles.cat}
             src="/assets/gatitriste.png" // URL relativa para imágenes en "public"
             alt="Carrito vacío"
            />
         </div>
       ) : (
         <>
-          <ul className="product-grid">
+          <ul className={styles["product-grid"]}>
             {state.items.map((item) => (
               <li
                 key={item.id}
-                className="product-card"
+                className={styles["product-card"]}
               >
                 {item.image && (
                   <img
-                  className="cart-item-image"
+                  className={styles["cart-item-image"]}
                     src={item.image}
                     alt={item.name}
                   />
                 )}
                 <div>
                   <h3>{item.name}</h3>
-                  <p className="price">
+                  <p className={styles.price}>
                     ${item.price} x {item.quantity}
                   </p>
-                  <button onClick={() => handleRemove(item.id, dispatch)}>
+                  <button className={styles.remover} onClick={() => handleRemove(item.id, dispatch)}>
                     Remover
                   </button>
                 </div>
               </li>
             ))}
           </ul>
-          <h3 className="total">Total: ${total.toFixed(2)}</h3>
+          <h3 className={styles.total}>Total: ${total.toFixed(2)}</h3>
           <div
           >
-            <button onClick={() => handleClearCart(dispatch)}>
+            <button className={styles.limpiar} onClick={() => handleClearCart(dispatch)}>
               Limpiar carrito
             </button>
-            <button onClick={() => navigate('/payment')}>Pagar</button>
+            <button className={styles.pagar} onClick={() => navigate('/payment')}>Pagar</button>
           </div>
         </>
       )}
